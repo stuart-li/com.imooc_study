d64b7beb14283b93ef3229b53110e0f39480d912
root <root@163.com>
2021-04-01 02:19:07 +0800

    feat:在git仓库的dev分支上传了第一个Java程序

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,9 +2,13 @@ public class GitTest {
     public void testGit(){
         System.out.println("这是测试git的Java程序");
     }
+    public void writeJava(){
+        System.out.println("ahahhah");
+    }
 
     public static void main(String[] args) {
         GitTest gitTest=new GitTest();
         gitTest.testGit();
+        gitTest.writeJava();
     }
 }

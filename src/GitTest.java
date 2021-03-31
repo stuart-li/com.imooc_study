e3a5dddb5504d60fa6b2ec9cf59d67c0a8cc1d35
lijianjun <lijianjun@163.com>
2021-04-01 01:52:47 +0800

    feat:在git仓库的dev分支上传了第一个Java程序

---
+++ src/GitTest.java
@@ -0,0 +1,10 @@
+public class GitTest {
+    public void testGit(){
+        System.out.println("这是测试git的Java程序");
+    }
+
+    public static void main(String[] args) {
+        GitTest gitTest=new GitTest();
+        gitTest.testGit();
+    }
+}
